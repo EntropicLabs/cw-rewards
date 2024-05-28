1b29a945fb779d6b5a9dd7d37f7fa399d8fac244
codehans <94654388+codehans@users.noreply.github.com>
2024-05-28 16:18:42 +0200

    hooked incentive

---
+++ contracts/cw-hook-incentive-rewards/src/execute.rs
@@ -0,0 +1,54 @@
+use cosmwasm_std::{Coin, DepsMut, MessageInfo, Response};
+use kujira::{Denom, KujiraMsg, KujiraQuery};
+use rewards_interfaces::{simple::WhitelistedRewards, ClaimRewardsMsg, DistributeRewardsMsg};
+use rewards_logic::util::{calculate_fee_msgs, calculate_fee_split};
+
+use crate::{contract::STATE_MACHINE, Config, ContractError};
+
+pub fn claim(
+    deps: DepsMut<KujiraQuery>,
+    info: MessageInfo,
+    msg: ClaimRewardsMsg,
+) -> Result<Response<KujiraMsg>, ContractError> {
+    rewards_logic::execute::claim(
+        STATE_MACHINE,
+        deps.storage,
+        &info.sender,
+        msg,
+        "rewards/hooked",
+    )
+    .map_err(ContractError::from)
+}
+
+pub fn distribute(
+    deps: DepsMut<KujiraQuery>,
+    info: MessageInfo,
+    config: Config,
+    msg: DistributeRewardsMsg,
+) -> Result<Response<KujiraMsg>, ContractError> {
+    if info.funds.is_empty() {
+        return Err(ContractError::ZeroRewards {});
+    }
+    if let WhitelistedRewards::Some(whitelist) = &config.whitelisted_rewards {
+        for Coin { denom, .. } in info.funds.iter() {
+            if !whitelist.contains(&Denom::from(denom)) {
+                return Err(ContractError::RewardNotWhitelisted {});
+            }
+        }
+    }
+
+    // Fee split
+    let mut rewards = info.funds;
+    let fees = calculate_fee_split(&mut rewards, &config.fees);
+    let msgs = calculate_fee_msgs(fees);
+
+    rewards_logic::execute::distribute_rewards(
+        STATE_MACHINE,
+        deps.storage,
+        info.sender,
+        rewards,
+        msg,
+        "rewards/hooked",
+    )
+    .map_or_else(|e| Err(e.into()), |res| Ok(res.add_messages(msgs)))
+}
--- packages/rewards-interfaces/src/lib.rs
+++ packages/rewards-interfaces/src/lib.rs
@@ -2,7 +2,8 @@ mod msg;
 pub use msg::*;
 
 pub mod claiming;
+pub mod hooked;
+pub mod hooked_incentive;
 pub mod incentive;
 pub mod permissioned_incentive;
 pub mod simple;
-pub mod hooked;
