5ed3704378bfc1879bbbdcd98702e37e1b262163
Amit Prasad <mail@amitprasad.dev>
2024-06-01 13:10:13 -0400

    move RewardsError to interfaces

--- packages/rewards-interfaces/src/lib.rs
+++ packages/rewards-interfaces/src/lib.rs
@@ -1,6 +1,9 @@
 mod msg;
 pub use msg::*;
 
+mod error;
+pub use error::RewardsError;
+
 pub mod claiming;
 pub mod incentive;
 pub mod permissioned_incentive;
--- packages/rewards-logic/src/lib.rs
+++ packages/rewards-logic/src/lib.rs
@@ -1,10 +1,7 @@
-mod error;
-
 pub mod execute;
 pub mod incentive;
 pub mod query;
 pub mod state_machine;
 pub mod util;
 
-pub use error::RewardsError;
 pub use state_machine::RewardsSM;
